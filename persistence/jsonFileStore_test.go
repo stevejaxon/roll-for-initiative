3968eb64441e0c8207acef6fab6f065453968d6a
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-11-12 21:25:17 -0800

    feat: adding a test for multiple creates

--- persistence/jsonFileStore_test.go
+++ persistence/jsonFileStore_test.go
@@ -25,3 +25,41 @@ func TestCreateFirstCharacter(t *testing.T) {
 		t.Fatalf("expected that it would be possible to create the first character in the DB: %v", err)
 	}
 }
+
+func TestCreateCalledMultipleTimes(t *testing.T) {
+	// Setup
+	const dbPath = "/tmp/multiple_characters.json"
+	db := &JSONCharacterStore{
+		DBFilePath: dbPath,
+	}
+	testCases := []*domain.Character{
+		&domain.Character{
+			Name:               "Test1",
+			InitiativeModifier: -1,
+		},
+		&domain.Character{
+			Name:               "Test2",
+			InitiativeModifier: 10,
+		},
+		&domain.Character{
+			Name:               "Test3",
+			InitiativeModifier: -10,
+		},
+		&domain.Character{
+			Name:               "Test4",
+			InitiativeModifier: 3,
+		},
+	}
+
+	// Test
+	for _, testCase := range testCases {
+		err := db.Create(testCase)
+
+		// Verify
+		if err != nil {
+			t.Fatalf("expected that it would be possible to create characters in the DB: %v", err)
+		}
+	}
+
+	// Verify
+}
